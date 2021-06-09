f8ee93d1197004a1e9b9c61103858de7965d361f
Sunny-Baishnab <72726626+Sunny-Baishnab@users.noreply.github.com>
2021-06-09 08:53:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation'
+import {createStackNavigator} from 'react-navigation-stack'
+import Home from './screens/Home'
+import Details from './screens/Details'
+
+export default class App extends Component {
+  render(){
+    return (
+      <AppContainer/>
+    )
+  }
+}
+
+const AppStackNavigator = createStackNavigator({
+  Home:{
+    screen:Home,
+    navigationOptions:{
+      headerShown:false
+    }
+  },
+  Details:{
+    screen:Details
+  }
+},
+{
+  initialRouteName:'Home'
+})
+
+const AppContainer = createAppContainer(AppStackNavigator)
